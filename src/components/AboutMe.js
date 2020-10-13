ee21a39279c5f89622aa8cae54939dbe6570c67f
do1con <kss7547@gmail.com>
2020-10-14 03:30:40 +0900

    About Me 반응형 레이아웃

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react'
 import styled from "styled-components";
 import { Motion, spring } from 'react-motion';
 import me from "../media/me.png";
-import { Row, Col, Steps } from "antd";
-import { MailTwoTone, PhoneTwoTone, DatabaseTwoTone, IdcardTwoTone } from '@ant-design/icons';
+import { Row, Col } from "antd";
+import { MailTwoTone, PhoneTwoTone, DatabaseTwoTone, IdcardTwoTone, RightOutlined, MinusOutlined } from '@ant-design/icons';
 
 function AboutMe(props) {
   
   const [ComponentLoadedState, setComponentLoadedState] = useState(false);
-  const { Step } = Steps;
 
   useEffect(() => {
     if(props.ScrollPosition === "aboutMe") setComponentLoadedState(true);
@@ -29,25 +28,77 @@ function AboutMe(props) {
         }
       </Motion>
       <hr/>
-      <Row style={{ marginTop: "50px" }}>
-        <Col xs={2} sm={4} md={6} lg={8} xl={7}>
-          <MyPicture />
-          <Profile>
-            <p><ProfileHead>김성수 </ProfileHead> SeongSoo Kim<br/>Web Front-End Developer</p>
-            <p><MailTwoTone /> kss7547@gmail.com</p>
-            <p><PhoneTwoTone /> 010-9902-4851</p>
-            <p><DatabaseTwoTone /> https://github.com/do1con</p>
-            <p><IdcardTwoTone /> 정보처리산업기사</p>
-          </Profile>
+      <Row style={{ marginTop: "50px" }} justify="center">
+        <Col xs={2} sm={4} md={24} lg={4} xl={6}>
+          <div style={{ width: '100%', height: '100%' }}>
+            <MyPicture src={me} alt="me" />
+            <Profile>
+              <p style={{ wordBreak: 'keep-all' }}><ProfileHead>김성수 </ProfileHead> SeongSoo Kim<br/>Web Front-End Developer</p>
+              <hr/>
+              <p style={{ wordBreak: 'break-all' }}><MailTwoTone /> kss7547@gmail.com</p>
+              <p style={{ wordBreak: 'break-all' }}><PhoneTwoTone /> 010-9902-4851</p>
+              <p style={{ wordBreak: 'break-all' }}><DatabaseTwoTone /> https://github.com/do1con</p>
+            </Profile>
+          </div>
         </Col>
-        <Col xs={2} sm={4} md={6} lg={8} xl={17}>
-          <Steps progressDot current={4} direction="vertical">
-            <Step title="학업" subTitle="2015.03 ~ 2017.02"  description="한국폴리텍2대학(인천캠퍼스) 컴퓨터정보과 졸업 학점 4.13" />
-            <Step title="교육" subTitle="2019.05 ~ 2019.11" description="한국아이티인재개발학원	멀티디바이스 웹 콘텐츠 개발자 과정 수료" />
-            <Step title="기타활동" description="국가인적자원개발컨소시엄 파이썬을 활용한 딥러닝 입문 과정" />
-            <Step title="기타활동" description="AWS Builders Korea AWS 100 입문 (Rookie 등급)" />
-            <Step description="계속해서 정진하고 있습니다. :)" />
-          </Steps>
+        <Col xs={2} sm={4} md={24} lg={12} xl={12}>
+          <div style={{ marginLeft: '20px' }}>
+            <ol>
+              <li>
+                <RightOutlined style={{ fontSize: "1.2rem", color: "rgb(51,157,255)" }} /> <span style={{ fontSize: "1.2rem" }}>한국폴리텍2대학 (인천캠퍼스)</span>
+                <ol>
+                  <li style={{ marginLeft: "25px" }}>
+                    2015.03 ~ 2017.02
+                  </li>
+                  <li style={{ marginLeft: "25px" }}>
+                    컴퓨터정보과 졸업
+                  </li>
+                  <li style={{ marginLeft: "25px" }}>
+                    졸업작품 : avr 기반 블루투스로 통신하여 조종하는 RC카와 조종 어플
+                  </li>
+                  <li style={{ marginLeft: "25px" }}>
+                    학점 4.13 / 4.5
+                  </li>
+                </ol>
+              </li>
+            </ol>
+            <ol>
+              <li>
+                <RightOutlined style={{ fontSize: "1.2rem", color: "rgb(51,157,255)" }} /> <span style={{ fontSize: "1.2rem" }}>한국아이티인재개발원</span>
+                <ol>
+                  <li style={{ marginLeft: "25px" }}>
+                    2019.05 ~ 2019.11
+                  </li>
+                  <li style={{ marginLeft: "25px" }}>
+                    멀티디바이스 웹 콘텐츠 개발자 과정 수료
+                  </li>
+                </ol>
+              </li>
+            </ol>
+            <ol>
+              <li>
+                <RightOutlined style={{ fontSize: "1.2rem", color: "rgb(51,157,255)" }} /> <span style={{ fontSize: "1.2rem" }}>차세대융합콘텐츠산업협회</span>
+                <ol>
+                  <li style={{ marginLeft: "25px" }}>
+                    2020.02.17 ~ 2020.02.19
+                  </li>
+                  <li style={{ marginLeft: "25px" }}>
+                    파이썬을 활용한 딥러닝 입문 과정 수료
+                  </li>
+                </ol>
+              </li>
+            </ol>
+            <ol>
+              <li>
+                <RightOutlined style={{ fontSize: "1.2rem", color: "rgb(51,157,255)" }} /> <span style={{ fontSize: "1.2rem" }}>AWS Builders Korea</span>
+                <ol>
+                  <li style={{ marginLeft: "25px" }}>
+                    AWS Builders Level 100 (Rookie)
+                  </li>
+                </ol>
+              </li>
+            </ol>
+          </div>
         </Col>
       </Row>
 
@@ -68,17 +119,13 @@ const AboutMeTitle = styled.h2`
   margin-top: 25px;
 `;
 
-const MyPicture = styled.div`
-  width: 200px;
-  height: 200px;
-  background-image: url(${me});
-  background-size: cover;
-  background-position: center, center;
-  border-radius: 50% 20% / 10% 40%;
+const MyPicture = styled.img`
+  height: 150px;
+  margin: 0 auto;
+  border-radius: 50% 50% / 50% 50%;
 `;
 
 const Profile = styled.p`
-  width: 250px;
   margin-top: 15px;
 `;
 
